24a409f6e3f2c4f9725d2e0154cd6760a887819f
Laura <32144109+Lukulu@users.noreply.github.com>
2021-11-24 22:18:53 +0100

    Update H2.c

--- 02_QR_Iteration/H2.c
+++ 02_QR_Iteration/H2.c
@@ -32,6 +32,8 @@ void printVector(int length, double* v) {
 	}
 }
 
+/*perform matrix multiplication of nxn matrices A, B and store
+the result in AB*/
 void matrix_mul(double* A, double* B, double* AB, int n){
 	/*initialize result matrix AB to zero matrix*/
 	for (int j = 0; j < n; j++)
@@ -149,11 +151,81 @@ void get_QR_decomposition(double* A, double* Q, int n, int m){
     /*reset vector x to the first column of the new matrix A*/
     for (int k = 0; k < n; k++) x[k] = A[(j + 1) * n + k];
 
-    /*write the normalized HH vector into thej-th column of A below the diagonal.*/
-    if (j != findMin(m - 1, n))
+    // /*write the normalized HH vector into thej-th column of A below the diagonal.*/
+    // if (j != findMin(m - 1, n))
+    // {
+    //   for (int k = (j + 1); k < n; k++) A[j * n + k] = v[k] / v[j];
+    // }
+  }
+  free(w);
+  free(x);
+  free(v);
+}
+
+/*calculate QR-decomposition of a band matrix A of dimension nxn with*/
+void get_QR_decomp_band_matrix(double* A, double* Q, double* R, int n){
+  /*allocate vector w of size n, and set it to the first column of Q*/
+  double* w = (double*)malloc(sizeof(double) * n);
+  for (int i = 0; i < n; i++) w[i] = Q[i];
+
+  /*buffer column vector of A at current iteration step*/
+  double* x = (double*)malloc(sizeof(double) * n);
+  /*set x to the first column of A to start the iteration*/
+  for (int i = 0; i < n; i++) x[i] = A[i];
+
+  /*buffer Householder vector of current iteration step*/
+  double* v = (double*)malloc(sizeof(double) * n);
+
+	/*set R = A and Q = I to start the iteration*/
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < n; i++) {
+			R[j*n+i] = A[j*n+i];
+			Q[j * n + i] = 0;
+			if (j == i) Q[j * n + i] = 1;
+		}
+	}
+
+  /*perform QR-decomposition: calculate HH-vectors and R*/
+  for (int j = 0; j < n; j++) //n iteration steps
+  {
+    double abs = abs_vec(R, j, n, 0); //absolute value of column vector j of A
+
+    /*calculate Householder vector v for iteration step j*/
+    for (int i = j; i < n; i++)
     {
-      for (int k = (j + 1); k < n; k++) A[j * n + k] = v[k] / v[j];
+      if (i == j) v[i] = R[j * n + i] + sgn(R[j * n + i]) * abs;
+      else     v[i] = R[j * n + i];
     }
+
+    //calculate H_j * A_j
+    double abs_v = abs_vec1(v, j, n, 1); //absolute value of HH vector v squared
+    for (int l = j; l < n; l++) //iterate over columns
+    {
+      for (int k = j; k < n; k++) //iterate over rows
+      {
+        for (int d = j; d < n; d++)
+        {
+          R[l * n + k] += -2 / abs_v * v[k] * v[d] * x[d];
+        }
+      }
+      /*set vector x to the next column of A*/
+      for (int k = 0; k < n; k++) x[k] = R[(l + 1) * n + k];
+    }
+
+    /*calculate matrix Q for each iteration step j*/
+    for (int l = 0; l < n; l++) //iterate over columns of Q
+    {
+      //set vector w to transposed column vector
+      for (int i = j; i < n; i++) w[i] = Q[i * n + l];
+      /*calculate next Q-matrix analogeously to part 1 above, taking the transposition
+      for the final result already into account*/
+      for (int k = j; k < n; k++)
+      {
+        for (int d = j; d < n; d++) Q[k * n + l] += -2 / abs_v * v[k] * v[d] * w[d];
+      }
+    }
+    /*reset vector x to the first column of the new matrix A*/
+    for (int k = 0; k < n; k++) x[k] = R[(j + 1) * n + k];
   }
   free(w);
   free(x);
@@ -164,9 +236,7 @@ void get_QR_decomposition(double* A, double* Q, int n, int m){
 int main(int argc, char** argv)
 {
 	double* A; //input matrix A
-	double* Q; //matrix Q
 	int n = 0; //number of rows/columns
-  // double eps = 0.000001 //check whether the HH-vector is zero, i.e. smaller than eps
 
 	/*----------------------Read Matrix from Command Line-----------------------*/
 	// /*Read number of rows n, number of columns m and the matrix A itself
@@ -221,19 +291,22 @@ int main(int argc, char** argv)
 
 
 	/*----------------------Tridiagonalization----------------------------------*/
-  Q = (double*)malloc(sizeof(double) * n * n);
-  /*set Q to the nxn unit matrix*/
+	double* P; //matrix P
+	double eps = 0.000001; //check whether the HH-vector is zero, i.e. smaller than eps
+
+  P = (double*)malloc(sizeof(double) * n * n); //store orthogonal transformation
+  /*set P to the nxn unit matrix*/
 	for (int j = 0; j < n; j++)
 	{
 		for (int i = 0; i < n; i++)
 		{
-			Q[j * n + i] = 0;
-			if (j == i) Q[j * n + i] = 1;
+			P[j * n + i] = 0;
+			if (j == i) P[j * n + i] = 1;
 		}
 	}
 
   double* w = (double*)malloc(sizeof(double) * n);
-  for (int i = 0; i < n; i++) w[i] = Q[i];
+  for (int i = 0; i < n; i++) w[i] = P[i];
 
   /*buffer Householder vector of current iteration step*/
   double* v = (double*)malloc(sizeof(double) * n);
@@ -250,12 +323,20 @@ int main(int argc, char** argv)
   for (int j = 0; j < n-2; j++) //n-2 iteration steps
   {
     double abs = abs_vec(A, j+1, n, 0); //absolute value of part of column vector j of A
+		double sum = 0; //sum over elements of HH-vector
 
     /*calculate Householder vector v for iteration step j*/
 		v[j+1] = A[j * n + (j+1)] + sgn(A[j * n + (j+1)]) * abs;
-    for (int i = j+2; i < n; i++) v[i] = A[j * n + i];
-		printVector(n,v);
-		printf("\n");
+    for (int i = j+2; i < n; i++)
+		{
+			 v[i] = A[j * n + i];
+			 sum += fabs(v[i]);
+		}
+		/*catch the case whether the HH-vector is zero*/
+		if(sum < eps) {
+		  printf("Error! The HH-vector becomes the zero vector!");
+		  return 1;
+		}
 
     /*calculate P_j * A_j = A'_j*/
 		double abs_v = abs_vec1(v, j+1, n, 1); //absolute value of HH vector v squared
@@ -272,10 +353,7 @@ int main(int argc, char** argv)
       for (int k = 0; k < n; k++) x[k] = A[(l + 1) * n + k];
     }
 
-    printMatrix(n, n, A);
-
     /*calculate A'_j * P_j = P_j * A_j * P_j*/
-
     /*store first row vector of A' in y*/
     for (int l = 0; l < n; l++) y[l] = A[l*n];
 
@@ -292,19 +370,16 @@ int main(int argc, char** argv)
       for (int k = 0; k < n; k++) y[k] = A[k * n + (l + 1)];
     }
 
-    printMatrix(n, n, A);
-    printf("---------------------------------------------------------------\n");
-
-    /*calculate matrix Q=P for each iteration step j*/
-    for (int l = 0; l < n; l++) //iterate over columns of Q
+    /*calculate matrix P for each iteration step j*/
+    for (int l = 0; l < n; l++) //iterate over columns of P
     {
       //set vector w to transposed column vector
-      for (int i = j; i < n; i++) w[i] = Q[i * n + l];
-      /*calculate next Q-matrix analogeously to part 1 above, taking the transposition
+      for (int i = j; i < n; i++) w[i] = P[i * n + l];
+      /*calculate next P-matrix analogeously to part 1 above, taking the transposition
       for the final result already into account*/
       for (int k = j; k < n; k++)
       {
-        for (int d = j+1; d < n; d++) Q[k * n + l] += -2 / abs_v * v[k] * v[d] * w[d];
+        for (int d = j+1; d < n; d++) P[k * n + l] += -2 / abs_v * v[k] * v[d] * w[d];
       }
     }
 
@@ -319,51 +394,68 @@ int main(int argc, char** argv)
 
   }
 
+	/*print tridiagonalized matrix A*/
   printf("Resulting tridiagonal matrix:\n");
   printMatrix(n, n, A);
 
-  /*print orthogonal matrix Q (already transposed)*/
-  printf("\nResulting orthogonal matrix Q: \n");
-  printMatrix(n, n, Q);
+  /*print orthogonal matrix P*/
+  printf("\nResulting orthogonal matrix P: \n");
+  printMatrix(n, n, P);
 
-	/*check result*/
-	double* QA = (double*)malloc(sizeof(double) * n * n);
-	double* QAQ = (double*)malloc(sizeof(double) * n * n);
-	matrix_mul(Q, A, QA, n);
-	matrix_mul(QA, Q, QAQ, n);
+	/*check result by calculating P*A*P to get the original matrix back*/
+	double* PA = (double*)malloc(sizeof(double) * n * n);
+	double* PAP = (double*)malloc(sizeof(double) * n * n);
+	matrix_mul(P, A, PA, n);
+	matrix_mul(PA, P, PAP, n);
 	printf("\nOriginal Matrix A: \n");
-	printMatrix(n, n, QAQ);
-	free(QA);
-	free(QAQ);
-
-	/*---------------------Calculate QR Decomposition of A----------------------*/
-	// /*allocate nxn-matrix Q and set it to the unit matrix*/
-	// Q = (double*)malloc(sizeof(double) * n * n);
-	// for (int j = 0; j < n; j++)
-	// {
-	// 	for (int i = 0; i < n; i++)
-	// 	{
-	// 		Q[j * n + i] = 0;
-	// 		if (j == i) Q[j * n + i] = 1;
-	// 	}
-	// }
-  //
-	// /*perform QR-decomposition*/
-  // get_QR_decomposition(A, Q, n, m);
+	printMatrix(n, n, PAP);
+	free(PA);
+	free(PAP);
 	/*--------------------------------------------------------------------------*/
 
-	// /*print upper triangular matrix R with HH-vectors below its diagonal*/
-	// printf("\nResulting matrix upper triangular matrix R with HH-vectors:\n");
-	// printMatrix(n, m, A);
-  //
-	// /*print orthogonal matrix Q (already transposed)*/
-	// printf("\nResulting orthogonal matrix Q: \n");
-	// printMatrix(n, n, Q);
+	/*------------------------------QR-Iteration--------------------------------*/
+	/*allocate nxn-matrix Q and set it to the unit matrix*/
+	double* Qk = (double*)malloc(sizeof(double) * n * n);
+
+	double* Q = (double*)malloc(sizeof(double) * n * n);
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			Q[j * n + i] = 0;
+			if (j == i) Q[j * n + i] = 1;
+		}
+	}
+
+	double* R = (double*)malloc(sizeof(double) * n * n);
+
+	int k = 0;
+	while (k < 50)
+	{
+		get_QR_decomp_band_matrix(A, Qk, R, n);
+		printf("Matrix R:\n");
+		printMatrix(n, n, R);
+		printf("Matrix Q:\n");
+		printMatrix(n, n, Qk);
+
+		matrix_mul(R, Qk, A, n);
+		// matrix_mul(Qk, Q, Q, n);
+		k++;
+	}
+	/*print result QR iteration*/
+	printf("\nResulting matrix QR-Iteration:\n");
+	printMatrix(n, n, A);
+
+	/*perform QR-decomposition*/
+  // get_QR_decomposition(A, Q1, n, m);
+	/*--------------------------------------------------------------------------*/
 
 	/*free allocated memory*/
-
-	free(A);
+	free(Qk);
 	free(Q);
+	free(R);
+	free(A);
+	free(P);
   free(v);
   free(x);
   free(y);
