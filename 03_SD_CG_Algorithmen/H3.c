9f0c2cf03c0f9f459e643926c4348a26e6fef352
Lukulu <s6lazywi@uni-bonn.de>
2021-12-16 18:44:42 +0100

    zweiter Teil ALgorithmus hinzugefuegt-fehlt nur noch testen

--- 03_SD_CG_Algorithmen/H3.c
+++ 03_SD_CG_Algorithmen/H3.c
@@ -1,8 +1,7 @@
-/**/
+/*@author: Yuliya Kryvistkaya, Jiani Shen, Laura Zywietz Rolón*/
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
-# include <stdbool.h>
 
 /*compile command:
 gcc -Wall -pedantic -Wextra -Wno-unused-parameter H3.c -o ex -lm */
@@ -34,10 +33,10 @@ void printVector(int length, double* v) {
 }
 
 /*perform matrix multiplication of nxm matrix A and
-mxn matrix B and store the result in nxn matrix AB*/
-void matrix_matrix_mul(double* A, double* B, double* AB, int n, int m){
+mxl matrix B and store the result in nxl matrix AB*/
+void matrix_matrix_mul(double* A, double* B, double* AB, int n, int m, int l){
 	/*initialize result matrix AB to zero matrix*/
-	for (int j = 0; j < n; j++)
+	for (int j = 0; j < l; j++)
 	{
 		for (int i = 0; i < n; i++)
 		{
@@ -46,13 +45,13 @@ void matrix_matrix_mul(double* A, double* B, double* AB, int n, int m){
 	}
 
 	/*perform matrix multiplication AB = A*B*/
-	for (int j = 0; j < n; j++)
+	for (int j = 0; j < l; j++)
 	{
 		for (int i = 0; i < n; i++)
 		{
 			for (int d = 0; d < m; d++)
 			{
-				AB[j*n+i] += A[d*m+i]*B[j*n+d];
+				AB[j*n+i] += A[d*m+i]*B[j*l+d];
 			}
 		}
 	}
@@ -77,6 +76,8 @@ void matrix_vector_mul(double* A, double* x, double* Ax, int n){
 	}
 }
 
+/*caculate euclidean scalar product of two vectors vec1 and vec2
+ of length n*/
 double euclidean_scp(double* vec1, double* vec2, int length){
 	double res = 0;
 	for (int i = 0; i < length; i++) {
@@ -85,204 +86,18 @@ double euclidean_scp(double* vec1, double* vec2, int length){
 	return res;
 }
 
-// /*determine the sign of a given number a*/
-// int sgn(double a) {
-// 	if (a >= 0) return 1;
-// 	else return -1;
-// }
-//
-// /*calculate minimum of two numbers a, b*/
-// int findMin(int a, int b) {
-// 	if (a < b) return a;
-// 	else return b;
-// }
-
-/*determine absolute value of column vector at pos of A
-of length n according to the Euclidean norm.
-If squared = 1: return the squared value to avoid
-its calculation afterwars by squaring the root.*/
-double abs_vec(double* A, int pos, int n, int squared) {
-	double res = 0;
-	for (int i = pos; i < n; i++) {
-		res += A[pos * n + i] * A[pos * n + i];
-	}
-	if (squared) return res;
-	else return sqrt(res);
-}
-
-double abs_vec2(double* A, int pos, int n, int squared) {
-	double res = 0;
-	for (int i = pos; i < n-1; i++) {
-		res += A[pos * n + i+1] * A[pos * n + i+1];
-		// printf("i=%d: %lf\n", i, A[pos * n + i+1]);
-	}
-	// printf("\n");
-	if (squared) return res;
-	else return sqrt(res);
-}
-
-/*determine the absolute value of a vector v of length length-pos.
-If squared = 1: return the squared value to avoid
-its calculation afterwars by squaring the root.*/
-double abs_vec1(double* v, int pos, int length, int squared) {
-	double res = 0;
-	for (int i = pos; i < length; i++) {
-		res += v[i] * v[i];
-	}
-	if (squared) return res;
-	else return sqrt(res);
-}
-
-// /*calculate QR-decomposition of a matrix A of dimension nxm*/
-// void get_QR_decomposition(double* A, double* Q, int n, int m){
-//   /*allocate vector w of size n, and set it to the first column of Q*/
-//   double* w = (double*)malloc(sizeof(double) * n);
-//   for (int i = 0; i < n; i++) w[i] = Q[i];
-//
-//   /*buffer column vector of A at current iteration step*/
-//   double* x = (double*)malloc(sizeof(double) * n);
-//   /*set x to the first column of A to start the iteration*/
-//   for (int i = 0; i < n; i++) x[i] = A[i];
-//
-//   /*buffer Householder vector of current iteration step*/
-//   double* v = (double*)malloc(sizeof(double) * n);
-//
-//   /*perform QR-decomposition: calculate HH-vectors and R*/
-//   for (int j = 0; j <= findMin(m - 1, n); j++) //min(m-1, n)+1 iteration steps
-//   {
-//     double abs = abs_vec(A, j, n, 0); //absolute value of column vector j of A
-//
-//     /*calculate Householder vector v for iteration step j*/
-//     for (int i = j; i < n; i++)
-//     {
-//       if (i == j) v[i] = A[j * n + i] + sgn(A[j * n + i]) * abs;
-//       else     v[i] = A[j * n + i];
-//     }
-//
-//     //calculate H_j * A_j
-//     double abs_v = abs_vec1(v, j, n, 1); //absolute value of HH vector v squared
-//     for (int l = j; l < m; l++) //iterate over columns
-//     {
-//       for (int k = j; k < n; k++) //iterate over rows
-//       {
-//         for (int d = j; d < n; d++)
-//         {
-//           A[l * n + k] += -2 / abs_v * v[k] * v[d] * x[d];
-//         }
-//       }
-//       /*set vector x to the next column of A*/
-//       for (int k = 0; k < n; k++) x[k] = A[(l + 1) * n + k];
-//     }
-//
-//     /*calculate matrix Q for each iteration step j*/
-//     for (int l = 0; l < n; l++) //iterate over columns of Q
-//     {
-//       //set vector w to transposed column vector
-//       for (int i = j; i < n; i++) w[i] = Q[i * n + l];
-//       /*calculate next Q-matrix analogeously to part 1 above, taking the transposition
-//       for the final result already into account*/
-//       for (int k = j; k < n; k++)
-//       {
-//         for (int d = j; d < n; d++) Q[k * n + l] += -2 / abs_v * v[k] * v[d] * w[d];
-//       }
-//     }
-//
-//     /*reset vector x to the first column of the new matrix A*/
-//     for (int k = 0; k < n; k++) x[k] = A[(j + 1) * n + k];
-//
-//     // /*write the normalized HH vector into thej-th column of A below the diagonal.*/
-//     // if (j != findMin(m - 1, n))
-//     // {
-//     //   for (int k = (j + 1); k < n; k++) A[j * n + k] = v[k] / v[j];
-//     // }
-//   }
-//   free(w);
-//   free(x);
-//   free(v);
-// }
-//
-// /*calculate QR-decomposition of a band matrix A of dimension nxn with*/
-// void get_QR_decomp_band_matrix(double* A, double* Q, double* R, int n){
-//   /*allocate vector w of size n, and set it to the first column of Q*/
-//   double* w = (double*)malloc(sizeof(double) * n);
-//   for (int i = 0; i < n; i++) w[i] = Q[i];
-//
-//   /*buffer column vector of A at current iteration step*/
-//   double* x = (double*)malloc(sizeof(double) * n);
-//   /*set x to the first column of A to start the iteration*/
-//   for (int i = 0; i < n; i++) x[i] = A[i];
-//
-//   /*buffer Householder vector of current iteration step*/
-//   double* v = (double*)malloc(sizeof(double) * n);
-//
-// 	/*set R = A and Q = I to start the iteration*/
-// 	for (int j = 0; j < n; j++) {
-// 		for (int i = 0; i < n; i++) {
-// 			R[j*n+i] = A[j*n+i];
-// 			Q[j * n + i] = 0;
-// 			if (j == i) Q[j * n + i] = 1;
-// 		}
-// 	}
-//
-//   /*perform QR-decomposition: calculate HH-vectors and R*/
-//   for (int j = 0; j < n; j++) //n iteration steps
-//   {
-//     double abs = abs_vec(R, j, n, 0); //absolute value of column vector j of A
-//
-//     /*calculate Householder vector v for iteration step j*/
-//     for (int i = j; i < n; i++)
-//     {
-//       if (i == j) v[i] = R[j * n + i] + sgn(R[j * n + i]) * abs;
-//       else     v[i] = R[j * n + i];
-//     }
-//
-//     //calculate H_j * A_j
-//     double abs_v = abs_vec1(v, j, n, 1); //absolute value of HH vector v squared
-//     for (int l = j; l < n; l++) //iterate over columns
-//     {
-//       for (int k = j; k < n; k++) //iterate over rows
-//       {
-//         for (int d = j; d < n; d++)
-//         {
-//           R[l * n + k] += -2 / abs_v * v[k] * v[d] * x[d];
-//         }
-//       }
-//       /*set vector x to the next column of A*/
-//       for (int k = 0; k < n; k++) x[k] = R[(l + 1) * n + k];
-//     }
-//
-//     /*calculate matrix Q for each iteration step j*/
-//     for (int l = 0; l < n; l++) //iterate over columns of Q
-//     {
-//       //set vector w to transposed column vector
-//       for (int i = j; i < n; i++) w[i] = Q[i * n + l];
-//       /*calculate next Q-matrix analogeously to part 1 above, taking the transposition
-//       for the final result already into account*/
-//       for (int k = j; k < n; k++)
-//       {
-//         for (int d = j; d < n; d++) Q[k * n + l] += -2 / abs_v * v[k] * v[d] * w[d];
-//       }
-//     }
-//     /*reset vector x to the first column of the new matrix A*/
-//     for (int k = 0; k < n; k++) x[k] = R[(j + 1) * n + k];
-//   }
-//   free(w);
-//   free(x);
-//   free(v);
-// }
-
-/*print the transposed matrix At of A*/
-void transposeMatrix(int n, double *A, double* At){
+/*print the transposed mxn matrix At of nxm matrix A*/
+void transpose_matrix(double *A, double* At, int n, int m){
 	/*initialize result matrix AB to zero matrix*/
 	for (int j = 0; j < n; j++)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < m; i++)
 		{
 			At[j*n+i] =0;
 		}
 	}
 
- for (int i = 0; i < n; i++)
+ for (int i = 0; i < m; i++)
  {
 	 for (int j = 0; j < n; j++)
 	 {
@@ -290,7 +105,35 @@ void transposeMatrix(int n, double *A, double* At){
 	 }
  }
 }
-/*----------------------------------------------------------------------------*/
+
+/*calculate (R,R) and (P, AP) and print it to the command line*/
+void print_results_algorithms(double* R, double* P, double* A, int n, int K){
+	double* Rt = (double*)malloc(sizeof(double) * K * n);
+	transpose_matrix(R, Rt, n, K);
+
+	double* Pt = (double*)malloc(sizeof(double) * K * n);
+	transpose_matrix(P, Pt, n, K);
+
+	double* AP = (double*)malloc(sizeof(double) * n * K);
+	double* RtR = (double*)malloc(sizeof(double) * K * K);
+	double* PtAP = (double*)malloc(sizeof(double) * K * K);
+
+	matrix_matrix_mul(Rt, R, RtR, K, n, K);
+	matrix_matrix_mul(A, P, AP, n, n, K);
+	matrix_matrix_mul(Pt, AP, PtAP, K, n, K);
+
+	printf("The scalar product of matrices (R,R) has the form: \n");
+	printMatrix(K, K, RtR);
+	printf("The scalar product of matrices (P,AP) has the form: \n");
+	printMatrix(K, K, PtAP);
+
+	free(Rt);
+	free(Pt);
+	free(AP);
+	free(RtR);
+	free(PtAP);
+}
+/*-------------------------------------------------------------------------------*/
 
 int main(int argc, char** argv)
 {
@@ -387,8 +230,7 @@ int main(int argc, char** argv)
 	// 		A[j*n+i] = 1/(i+j+1);
 	// 	}
 	// }
-
-	/*----------------------Steepest Decent ----------------------------------*/
+	/*---------------------------algorithms-----------------------------------*/
 	/*define matrix P of search directions: P = (p_1,p_2,...,p_K)
 	and the matrix R of residues: R = (r_1,r_2,...,r_K)*/
 	double *P;
@@ -405,27 +247,28 @@ int main(int argc, char** argv)
 	double* r;
 	double* Ax;
 	double* p;
-	double norm_r = 0, norm_b = 0;
 
-	Ax = (double*)malloc(sizeof(double)*n);
-	matrix_vector_mul(A, x, Ax, n);
-
-	r = (double*)malloc(sizeof(double)*n);
-	for (int i = 0; i < n; i++) {
-		r[i] = b[i] - Ax[i];
-	}
-	free(Ax);
-
-	/*calculate norm of r*/
-	norm_r = euclidean_scp(r, r, n);
-	norm_r = sqrt(norm_r);
-
-	/*calculate norm of b*/
-	norm_b = euclidean_scp(b, b, n);
-	norm_b = sqrt(norm_b);
-
+	/*----------------------Steepest Decent ----------------------------------*/
 	if(alg_num == 1)
 	{
+		double norm_r = 0, norm_b = 0;
+		Ax = (double*)malloc(sizeof(double)*n);
+		matrix_vector_mul(A, x, Ax, n);
+
+		r = (double*)malloc(sizeof(double)*n);
+		for (int i = 0; i < n; i++) {
+			r[i] = b[i] - Ax[i];
+		}
+		free(Ax);
+
+		/*calculate norm of r*/
+		norm_r = euclidean_scp(r, r, n);
+		norm_r = sqrt(norm_r);
+
+		/*calculate norm of b*/
+		norm_b = euclidean_scp(b, b, n);
+		norm_b = sqrt(norm_b);
+
 		int k = 0;
 		while (norm_r > eta*norm_b || k <= K)
 		{
@@ -441,73 +284,50 @@ int main(int argc, char** argv)
 				R[k*K+i] = r[i];
 			}
 			k++;
+			free(p);
 		}
 
 		/*calculate (R,R) and (P, AP) and print it to the command line*/
-		double* Rt = (double*)malloc(sizeof(double) * K * n);
-		transposeMatrix(n, R, Rt);
-		double* Pt = (double*)malloc(sizeof(double) * K * n);
-		transposeMatrix(n, P, Pt);
-		double* AP = (double*)malloc(sizeof(double) * n * K);
-		double* RtR = (double*)malloc(sizeof(double) * K * K);
-		double* PtAP = (double*)malloc(sizeof(double) * K * K);
-		// matrix_matrix_mul(Rt, R, RtR, K, n);
-		// matrix_matrix_mul(A, P, AP, n, K);
-		// matrix_matrix_mul(Pt, AP, PtAP, n, n);
-
-		printf("The scalar product of matrices (R,R) has the form: \n");
-		printMatrix(K, K, RtR);
-		printf("The scalar product of matrices (P,AP) has the form: \n");
-		printMatrix(K, K, PtAP);
-
-		free(Rt);
-		free(Pt);
-		free(AP);
-		free(RtR);
-		free(PtAP);
+		print_results_algorithms(R, P, A, n, K);
 	}
 	/*----------------------Conjugate Gradient----------------------------------*/
 	if(alg_num == 2)
 	{
-		int k = 0;
-		while (norm_r > eta*norm_b)
-		{
-			p = (double*)malloc(sizeof(double)*n);
-			matrix_vector_mul(A, r, p, n);
+		/*initialize r (residual) and x for the CG algorithm with r = b and x = 0*/
+		double* new_r;
+		double* Ap;
+		new_r = (double*)malloc(sizeof(double)*n);
+		r = (double*)malloc(sizeof(double)*n);
+		p = (double*)malloc(sizeof(double)*n);
+		for (int i = 0; i < n; i++) {
+			r[i] = b[i];
+			x[i] = 0;
+			p[i] = r[i];
+		}
 
-			double alpha = euclidean_scp(r,r,n) / euclidean_scp(p,r,n);
+		Ap = (double*)malloc(sizeof(double)*n);
+		int k = 0;
+		while (k < K)
+		{
+			matrix_vector_mul(A, p, Ap, n);
+			double alpha = euclidean_scp(r,r,n) / euclidean_scp(Ap,p,n);
 			for (int i = 0; i < n; i++)
 			{
-				x[i] += alpha*r[i];
-				r[i] -= alpha*p[i];
+				x[i] += alpha*p[i];
+				new_r[i] = r[i] - alpha*Ap[i];
+				p[i] = new_r[i] + euclidean_scp(new_r,new_r,n)/euclidean_scp(r,r,n)*p[i];
 				P[k*K+i] = p[i];
-				R[k*K+i] = r[i];
+				R[k*K+i] = new_r[i];
+				r[i] = new_r[i];
 			}
 			k++;
+
+			free(new_r);
+			free(Ap);
 		}
 
 		/*calculate (R,R) and (P, AP) and print it to the command line*/
-		double* Rt = (double*)malloc(sizeof(double) * K * n);
-		transposeMatrix(n, R, Rt);
-		double* Pt = (double*)malloc(sizeof(double) * K * n);
-		transposeMatrix(n, P, Pt);
-		double* AP = (double*)malloc(sizeof(double) * n * K);
-		double* RtR = (double*)malloc(sizeof(double) * K * K);
-		double* PtAP = (double*)malloc(sizeof(double) * K * K);
-		// matrix_matrix_mul(Rt, R, RtR, K, n);
-		// matrix_matrix_mul(A, P, AP, n, K);
-		// matrix_matrix_mul(Pt, AP, PtAP, n, n);
-
-		printf("The scalar product of matrices (R,R) has the form: \n");
-		printMatrix(K, K, RtR);
-		printf("The scalar product of matrices (P,AP) has the form: \n");
-		printMatrix(K, K, PtAP);
-
-		free(Rt);
-		free(Pt);
-		free(AP);
-		free(RtR);
-		free(PtAP);
+		print_results_algorithms(R, P, A, n, K);
 	}
 	/*--------------------------------------------------------------------------*/
 
